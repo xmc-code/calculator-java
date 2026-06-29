b51337d3fc7eba50d61aba889c93b20e20daf908
xmc-code <3759810791@qq.com>
2026-06-29 08:42:17 +0800

    feat:四则运算代码改进，增加scanner.close()

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args){
-// 读取两个数字和一个运算符
+        // 读取两个数字和一个运算符
         Scanner scanner = new Scanner(System.in );
-        double num1;
+        double num1,num2;
         char op;
-        double num2;
+
         try {
             // 尝试读取第一个数字
             System.out.println("输入第一个数字: ");
+
             num1 = scanner.nextDouble();
-
             System.out.println("输入运算符 (+, -, *, /): ");
+
             op = scanner.next().charAt(0);
-
             System.out.println("输入第二个数字: ");
-            num2 = scanner.nextDouble();
 
+            num2 = scanner.nextDouble();
         } catch (java.util.InputMismatchException e) {
             // 如果用户输入的不是数字，就进入这个异常处理
             System.out.println("【异常捕获】错误：您必须输入合法的数字！程序终止。");
@@ -27,46 +27,37 @@ public class Calculator {
         }
 
         // 根据运算符计算结果
-        double result = -1;
+        double result;
+        boolean validOp = true;
         switch (op) {
             case '+':
                 result = num1 + num2;
+                System.out.println("运算类型：加法");
                 break;
             case '-':
                 result = num1 - num2;
+                System.out.println("运算类型：减法");
                 break;
             case '*':
                 result = num1 * num2;
+                System.out.println("运算类型：乘法");
                 break;
             case '/':
                 if (num2 != 0){
                     result = num1 / num2;
+                    System.out.println("运算类型：除法");
                 } else {
                     System.out.println("错误：除数不能为零。");
+                    scanner.close();
                     return;
                 }
                 break;
-        }
-
-        // char op = '+' ;
-        switch (op) {
-            case '+':
-                System.out.println("加法");
-                break;
-            case '-':
-                System.out.println("减法");
-                break;
-            case '*':
-                System.out.println("乘法");
-                break;
-            case '/':
-                System.out.println("除法");
-                break;
             default:
-                System.out.println("未知运算符");
+                System.out.println("错误：无效运算符‘"+op+"',仅支持 +，-，*，/");
+                scanner.close();
                 return;
         }
-
-        System.out.println("结果：" + result);
+        System.out.printf("计算结果：%.2f\n",result);
+        scanner.close();
     }
 }
